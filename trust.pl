14b01f6bfaf3b2adcaea59a4ae2747ac026e8dc0
Josh <carjo259@student.otago.ac.nz>
2023-12-06 14:31:30 +1300

    Updating trust or confidence in claims now recomputes beliefs, though this calculation is inefficient and needs to be optimised in the future.

--- trust.pl
+++ trust.pl
@@ -1,7 +1,7 @@
 :- use_module(library(heaps)).
 :- dynamic trust/3.                     % trust(Trustor, Trustee, Negative_Log_Trust)
 :- dynamic belief/3.                    % belief(Believer, Belief, Confidence)
-:- dynamic optimistic/0.                % Should not be altered if narrative partially computed!
+:- dynamic optimistic/0.                % Should not be altered during computation of the narrative!
 
 mode(Sign) :-
     \+ optimistic
@@ -16,21 +16,42 @@ update_trust(Trustor, Trustee, Trust) :-
     % Convert the (new) trust level into negative log trust; Logarithms have the property that log(a * b) = log(a) + log(b)
     % which is useful because Dijkstra's algorithm uses addition (not multiplication) for calculating path length
     % This also means we need Trust to have the right domain
-    (Trust > 0, Trust =< 1)
-        -> (mode(Sign), Log_Trust is Sign * log(Trust),
-            assert(trust(Trustor, Trustee, Log_Trust)))
-        % log(0) is undefined, so don't re-assert trust.
-        ; true.
+    (
+        (Trust > 0, Trust =< 1)
+            -> (mode(Sign), Log_Trust is Sign * log(Trust),
+                assert(trust(Trustor, Trustee, Log_Trust)))
+            % trust is 0, so don't re-assert.
+            ; true
+    ),
+    % Need to recalculate relevant confidence values of beliefs
+    forall(
+        % For every belief of Trustee,
+        (belief(Trustee, Claim, _)),
+        (
+            % Redo the computation of confidence
+            Claim = claim(Claimant, Statement, Time),
+            belief(Claimant, Claim, Claimant_Confidence),
+            update_claim(Claimant, Statement, Claimant_Confidence, Time)
+        )).
 
-create_claim(Entity, Claim, Confidence) :-
+% Claimant      - The entity who Claimants the claim to exist
+% Statement     - The statement being asserted / claimed by Claimant
+% Confidence    - How confident is the claimant in this claim?
+% Time          - When does the claim apply? i.e. claims can be made about the past
+update_claim(Claimant, Statement, Confidence, Time) :-
+    % Get rid of all the beliefs in this claim and recompute the beliefs through the (new) trust graph
+    % This is inefficient but simple and it ensures correctness
+    retractall(belief(_Believer, Claim, _)),
     % Make sure Confidence is within (0, 1] (cannot include zero due to log(0) being undefined)
     (Confidence > 0, Confidence =< 1)
         ->  (% Initialise the heap
             list_to_heap([], Heap),
+            % Store information in Claim about who created it, the statement it represents, and the time it holds / held
+            Claim = claim(Claimant, Statement, Time),
             % Dijkstra's algorithm
             mode(Sign), Log_Confidence is Sign * log(Confidence),
-            propagate_claim(node(Entity, Claim, Log_Confidence), Heap))
-
+            propagate_claim(node(Claimant, Claim, Log_Confidence), Heap))
+        % Otherwise, Confidence is zero (or an inappropriate value); it has effectively been retracted
         ;   true.
 
 propagate_claim(Current_Node, Heap_0) :- 
@@ -83,15 +104,38 @@ get_next_node(Heap_0, Next_Node, Heap_1) :-
     ; fail.
 
 
-% Removing old trust / belief relations
 
-:- retractall(belief(_, _, _)).
-:- retractall(trust(_, _, _)).
-:- update_trust(e2, e1, 0.5).
-:- update_trust(e3, e2, 0.5).
-:- update_trust(e3, e1, 0.75).
-:- update_trust(e2, e3, 0.75).
+print_beliefs :-
+    write("Beliefs:"),nl,
+    forall(
+        belief(X, Y, Z), 
+        (
+            Y = claim(Claimant, Statement, Time),
+            tab(4), write(X), write(" believes "),
+            write(Statement), write(" from "), write(Claimant),
+            write(" about time "), write(Time), write(", with confidence "), write(Z), nl
+        )
+    ).
 
-% Someone generates a claim
 
-?- create_claim(e1, claim, 1).
+% A small narrative for illustrative purposes
+
+?- retractall(belief(_, _, _)).
+?- retractall(trust(_, _, _)).
+
+?- update_trust(e2, e1, 0.5).
+?- update_trust(e3, e2, 0.5).
+?- update_trust(e3, e1, 0.75).
+?- update_trust(e2, e3, 0.75).
+
+?- update_claim(e1, property(object, "Q"), 1, 5).
+?- print_beliefs.
+?- update_trust(e3, e4, 0.5).
+?- update_trust(e4, e1, 0.2).
+?- print_beliefs.
+?- update_trust(e5, e4, 1).
+?- print_beliefs.
+?- update_claim(e1, property(object, "Q"), 0, 5).
+?- print_beliefs.
+?- update_claim(e1, property(object, "Q"), 0.9, 5).
+?- print_beliefs.
