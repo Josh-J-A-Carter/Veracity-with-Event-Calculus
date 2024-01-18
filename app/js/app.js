33a1146f77c1c96768db4a554df8c1b3164e567d
Josh <carjo259@student.otago.ac.nz>
2024-01-19 11:57:05 +1300

    Began functionality to choose which fluents, trust or specific judgements, are displayed

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,6 @@
 function updateDisplay() {
 	// Get the information relating to the selected timestamp, making sure that the selected timestamp actually exists
-	var current_info = timeline.find(slice => slice.timestamp === current_timestamp);
+	current_info = timeline.find(slice => slice.timestamp === current_timestamp);
 	if (current_info == undefined) {
 		current_info = timeline[0];
 		current_timestamp = current_info.timestamp;
@@ -10,89 +10,64 @@ function updateDisplay() {
 	slider.max = timeline.length;
 	slider.min = 1;
 
-	const eventsLabel = document.getElementById("events-label");
-	eventsLabel.innerHTML = `Events at timestamp ${current_timestamp}`;
-	const eventsText = document.getElementById("events");
-	eventsText.value = current_info.events
-			.map(event => jsonToPrologTerm(event))
-			.join("\n");
+	// const eventsLabel = document.getElementById("events-label");
+	// eventsLabel.innerHTML = `Events at timestamp ${current_timestamp}`;
+	// const eventsText = document.getElementById("events");
+	// eventsText.value = current_info.events
+	// 		.map(event => jsonToPrologTerm(event))
+	// 		.join("\n");
 
-	const fluentsLabel = document.getElementById("fluents-label");
-	fluentsLabel.innerHTML = `Fluents just after timestamp ${current_timestamp}`;
-	const fluentsText = document.getElementById("fluents");
-	fluentsText.value = current_info.fluents
-			.map(fluent => jsonToPrologTerm(fluent))
-			.join("\n");
+	// const fluentsLabel = document.getElementById("fluents-label");
+	// fluentsLabel.innerHTML = `Fluents just after timestamp ${current_timestamp}`;
+	// const fluentsText = document.getElementById("fluents");
+	// fluentsText.value = current_info.fluents
+	// 		.map(fluent => jsonToPrologTerm(fluent))
+	// 		.join("\n");
 
-	// Update textarea heights to fit text,
-	// making sure that events + fluents is not bigger than the maximum area
-	// by sharing the height by their ratios
-	maxHeight = window.innerHeight * 0.32;
-	minHeight = 15;
+	// // Update textarea heights to fit text,
+	// // making sure that events + fluents is not bigger than the maximum area
+	// // by sharing the height by their ratios
+	// maxHeight = window.innerHeight * 0.32;
+	// minHeight = 15;
 
-	var eventsHeight = eventsText.value.split(/\r|\r\n|\n/).length
-	var fluentsHeight = fluentsText.value.split(/\r|\r\n|\n/).length
+	// var eventsHeight = eventsText.value.split(/\r|\r\n|\n/).length
+	// var fluentsHeight = fluentsText.value.split(/\r|\r\n|\n/).length
 
-	sum = fluentsHeight + eventsHeight;
+	// sum = fluentsHeight + eventsHeight;
+	// if (sum == 0) sum = 1;
 	
-	newFluentsHeight = Math.max((fluentsHeight / sum) * maxHeight, minHeight);
-	newEventsHeight = Math.max((eventsHeight / sum) * maxHeight, minHeight);
+	// newFluentsHeight = Math.max((fluentsHeight / sum) * maxHeight, minHeight);
+	// newEventsHeight = Math.max((eventsHeight / sum) * maxHeight, minHeight);
 
-	newFluentsHeight = Math.min(newFluentsHeight, maxHeight - minHeight);
-	newEventsHeight = Math.min(newEventsHeight, maxHeight - minHeight);
+	// newFluentsHeight = Math.min(newFluentsHeight, maxHeight - minHeight);
+	// newEventsHeight = Math.min(newEventsHeight, maxHeight - minHeight);
 
-	fluentsText.style.height = `${newFluentsHeight}px`;
-	eventsText.style.height = `${newEventsHeight}px`;
+	// fluentsText.style.height = `${newFluentsHeight}px`;
+	// eventsText.style.height = `${newEventsHeight}px`;
 
+	// Update graph
+	updateGraph();
+}
 
-	//// Update graphical representation of the relevant fluents
+//// Update graphical representation of the relevant fluents
+function updateGraph() {
+	// Get the information relating to the selected timestamp, making sure that the selected timestamp actually exists
+	current_info = timeline.find(slice => slice.timestamp === current_timestamp);
+	if (current_info == undefined) {
+		current_info = timeline[0];
+		current_timestamp = current_info.timestamp;
+	}
 
-	// Collect the nodes and edges which will be contained in the set
-	// We also want to display their judgements when nodes are hovered over
-	var graph = new Set();
-	var judgements = current_info.fluents.filter(fluent => fluent.type == "judgement" && fluent.args.length == 3);
+	// If we don't have any information about fluents, return without drawing the graph.
+	if (current_info == undefined) return;
 
-	current_info.fluents
-		// We only want nodes involved in 'trust/2' fluents
-		.filter(fluent => fluent.type == "trust" && fluent.args.length == 2)
-		.forEach(fluent => {
+	// Default to displaying trust fluents
+	if (fluent_graph == undefined) fluent_graph = 'trust';
 
-			// Construct the hovertext for a given node, by looking up their current judgements
-			const constructHovertext = id => {
+	if (fluent_graph == 'trust') var obj = calculateTrustGraph();
+	// else var obj = calculateJudgementGraph();
 
-				var hovertext = judgements.reduce((text, judgement) => {
-					// This judgement doesn't relate to this node, so don't change the text
-					if (judgement.args[0] != id) return text;
-
-					// Otherwise, update the text by including this claim (in Prolog style)
-					var val = (Math.round(judgement.value * 100) / 100).toFixed(2);
-					var claim = jsonToPrologTerm(judgement.args[2]);
-					return `• ${claim}=${val}\n${text}`;
-				},
-				// Initialise the reduce function with the node's id
-				`\n${id}`);
-
-				return hovertext;
-			};
-
-			const name1 = fluent.args[0];
-			const hovertext1 = constructHovertext(name1);
-			graph.add({ data : { id : name1, hovertext : hovertext1 }});
-
-			const name2 = fluent.args[1];
-			const hovertext2 = constructHovertext(name2);
-			graph.add({ data : { id : name2, hovertext : hovertext2 }});
-			
-			// Add the edge to the graph
-			graph.add({
-				data : { 
-					id : `trust(${name1}, ${name2})`,
-					source : name1,
-					target : name2,
-					weight : fluent.value
-				}
-			})
-		});
+	const { graph, display_information } = obj;
 
 	// Display the graph
 	var cy = cytoscape({
@@ -107,7 +82,7 @@ function updateDisplay() {
 				'label': 'data(id)',
 				'font-size': '14px',
 				'text-wrap': 'ellipsis',
-				'text-max-width': '200px',
+				'text-max-width': '100px',
 				'text-overflow-wrap': 'anywhere',
 				'text-events': 'yes'
 			}
@@ -116,7 +91,6 @@ function updateDisplay() {
 			selector: 'node.hover',
 			style: {
 				'background-color': '#666',
-				'label': 'data(hovertext)',
 				'color': '#FFF',
 				'font-size': '16px',
 				'text-background-color': '#000',
@@ -124,8 +98,8 @@ function updateDisplay() {
 				'text-background-shape': 'round-rectangle',
 				'text-background-padding': '6px',
 				'text-wrap': 'wrap',
-				'text-max-width': '400px',
-				'text-overflow-wrap': 'whitespace',
+				'text-max-width': '200px',
+				'text-overflow-wrap': 'anywhere',
 				'text-events': 'yes'
 			}
 		},
@@ -143,7 +117,7 @@ function updateDisplay() {
 		layout: { 
 			name: 'klay',
 			klay: {
-				spacing: 75,
+				spacing: 80,
 				addUnnecessaryBendpoints: true,
 				aspectRatio: 2,
 				nodePlacement: 'LINEAR_SEGMENTS'
@@ -154,9 +128,101 @@ function updateDisplay() {
 	cy.minZoom(0.5);
 	cy.maxZoom(2);
 
-	// Ensure that the hovertext appears / disappears when the mouse moves onto or away from a node
-	cy.on("mouseover", "node", event => event.target.addClass("hover"));
+	// Ensure that the display text and hover effects are put into place
+	cy.on("mouseover", "node", event => {
+		event.target.addClass("hover");
+
+		var current_id = event.target.id();
+		var entry = display_information.find(node => node.id == current_id);
+		selected_node = current_id;
+
+		const selected_node_label = document.getElementById('selected-node-label');
+		const selected_node_text = document.getElementById('selected-node-text');
+	
+		if (fluent_graph == 'trust') selected_node_label.innerHTML = `Judgements held by ${selected_node}`;
+		else selected_node_label.innerHTML = "Proof tree for selected judgement";
+	
+		// Update the display area text
+		var entry = display_information.find(node => node.id == selected_node);
+		if (entry != undefined) selected_node_text.value = entry.text;
+		else selected_node_text.value = "";
+	});
 	cy.on("mouseout", "node.hover", event => event.target.removeClass("hover"));
+
+
+	// Update the display area label and text
+	const selected_node_label = document.getElementById('selected-node-label');
+	const selected_node_text = document.getElementById('selected-node-text');
+
+	if (selected_node == undefined) {
+		selected_node_label.innerHTML = "No node selected";
+		selected_node_text.value = "";
+		return;
+	}
+	else if (fluent_graph == 'trust') selected_node_label.innerHTML = `Judgements held by ${selected_node}`;
+	else selected_node_label.innerHTML = "Proof tree for selected judgement";
+
+	// Update the display area text
+	var entry = display_information.find(node => node.id == selected_node);
+	if (entry != undefined) selected_node_text.value = entry.text;
+	else selected_node_text.value = "";
+}
+
+function calculateTrustGraph() {
+	// Collect the nodes and edges in a set
+	var graph = new Set();
+
+	// We also want to display judgements when nodes are hovered over
+	var display_information = [];
+	var judgements = current_info.fluents.filter(fluent => fluent.type == "judgement" && fluent.args.length == 3);
+
+	// Iterate over all the trust/2 fluents to find the relevant nodes and edges
+	current_info.fluents
+		// We only want nodes involved in 'trust/2' fluents
+		.filter(fluent => fluent.type == "trust" && fluent.args.length == 2)
+		.forEach(fluent => {
+
+			// Construct the display text for this node
+			const constructDisplayText = id => {
+
+				var display_text = judgements.reduce((text, judgement) => {
+					// This judgement doesn't relate to this node, so don't change the text
+					if (judgement.args[0] != id) return text;
+					// Otherwise, update the text by including this judgement (in Prolog style)
+					return `${text}\n${jsonToPrologTerm(judgement)}`;
+				}, "");
+
+				return display_text;
+			};
+
+			const name_1 = fluent.args[0];
+			const display_text_1 = constructDisplayText(name_1);
+			graph.add({ data : { id : name_1 }});
+			display_information.push({
+				id : name_1,
+				text : display_text_1
+			});
+
+			const name_2 = fluent.args[1];
+			const display_text_2 = constructDisplayText(name_2);
+			graph.add({ data : { id : name_2 }});
+			display_information.push({
+				id : name_2,
+				text : display_text_2
+			});
+			
+			// Add the edge to the graph
+			graph.add({
+				data : { 
+					id : `trust(${name_1}, ${name_2})`,
+					source : name_1,
+					target : name_2,
+					weight : fluent.value
+				}
+			})
+		});
+	
+	return { graph, display_information };
 }
 
 function jsonToPrologTerm(json) {
@@ -178,6 +244,24 @@ function jsonToPrologTerm(json) {
 	value = (Math.round(json.value * 100) / 100).toFixed(2);
 	return `${functor}(${jsonifiedArguments})=${value}`;
 }
+
+// Callback function for when a button is clicked in order to change
+// the fluents being graphically displayed
+function changeFluentMode(button, fluent_graph_mode) {
+	// If this button is already in the "active" class, return early
+	const classList = Array.from(button.classList);
+	if (classList.includes('active')) return;
+
+	// Remove all of the sibling elements from the "active" class, and add it to this button
+	const siblingNodes = Array.from(button.parentNode.children);
+	siblingNodes.forEach(node => node.classList.remove('active'));
+	button.classList.add('active');
+
+	// Redraw the graph accordingly
+	fluent_graph = fluent_graph_mode;
+	updateGraph();
+}
+
 
 function moveSlider(event) {
 	if (timeline == undefined) return;
@@ -218,5 +302,12 @@ document.getElementById("update_eec").addEventListener("submit", submit);
 const slider = document.getElementById("timeline");
 slider.addEventListener("input", moveSlider);
 
+// Object holding all of the calculated events and fluents for a given narrative and set of rules
 var timeline = undefined;
+// The timestamp being displayed
 var current_timestamp = undefined;
+// The type of fluents being displayed in the graph
+var fluent_graph = 'trust';
+// The node in the graph that is currently selected
+// If a node is selected, its information (e.g. judgements) are displayed
+var selected_node = undefined;
